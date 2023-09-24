4c8c3d2707fed173f661f261830f419a1aaf476d
Chaitanya Kharya <kharyachaitanya@gmail.com>
2023-09-24 12:21:52 +0530

    fixes

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const multer = require("multer");
+
 const path = require("path");
 const { spawn } = require("child_process");
 
 const app = express();
 const port = 3000;
-
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 // Set up multer for file uploads
-const storage = multer.memoryStorage();
+// const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    // console.log(file);
+    cb(null, file.originalname);
+  },
+});
+
 const upload = multer({ storage });
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -24,31 +35,31 @@ app.post("/upload", upload.single("photo"), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: "No file uploaded." });
   }
-  // console.log("hello");
+  console.log("File Uploaded Successfully ", "No error");  
   // Create a Python child process to execute the TensorFlow script
-  const process = spawn("python", ["./predict.py"], {
-    stdio: "inherit",
-    input: req.file.buffer,
-  });
+  // const process = spawn("python", ["./predict.py"], {
+  //   stdio: "inherit",
+  //   input: req.file.buffer,
+  // });
 
-  let predictedDisease = "";
-  let treatment = "";
-  process.stdout.on("data", (data) => {
-    output = data.toString().split(",,");
-    predictedDisease += output[0];
-    treatment += output[1];
-  });
+  // let predictedDisease = "";
+  // let treatment = "";
+  // process.stdout.on("data", (data) => {
+  //   output = data.toString().split(",,");
+  //   predictedDisease += output[0];
+  //   treatment += output[1];
+  // });
 
-  process.on("exit", (code) => {
-    if (code === 0) {
-      res.json({
-        disease: predictedDisease.trim(),
-        treatment: treatment.trim(),
-      });
-    } else {
-      res.status(500).json({ error: "Prediction failed." });
-    }
-  });
+  // process.on("exit", (code) => {
+  //   if (code === 0) {
+  //     res.json({
+  //       disease: predictedDisease.trim(),
+  //       treatment: treatment.trim(),
+  //     });
+  //   } else {
+  //     res.status(500).json({ error: "Prediction failed." });
+  //   }
+  // });
 });
 
 app.listen(port, () => {
